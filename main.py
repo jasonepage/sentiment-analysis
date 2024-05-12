655a849922da3f8fe8eefadca2c651bb65c6f459
Jason Page <72579902+jasonepage@users.noreply.github.com>
2024-05-12 16:08:40 -0700

    First draft

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Description: This file is the main file for the program. It will run the program and call the necessary functions to track social media sentiment.
 
 import os, sys, time
-from sentiment import Sentiment
+from sentiment_old import Sentiment
 from twitter import Twitter
 from reddit import Reddit
 from database import Database
